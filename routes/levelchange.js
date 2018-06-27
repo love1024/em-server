84727141c90f88ccfe76753fe948c6b6b854ee57
Lovepreet Singh <lovepreet.singh@nagarro.com>
2018-06-27 16:19:03 +0530

    Added more apis

--- routes/levelchange.js
+++ routes/levelchange.js
@@ -3,11 +3,19 @@ const router = express.Router()
 
 const LevelChange = require('../models/level_change')
 
-/* GET ALL RESOURCES */
+/* GET ALL LEVELS */
 router.get('/', (req, res, next) => {
   LevelChange.find((err, resources) => {
     if (err) return next(err)
     res.json(resources)
+  })
+})
+
+/* ADD NEW LEVEL */
+router.post('/', (req, res, next) => {
+  LevelChange.create(req.body, (err, post) => {
+    if (err) return next(err)
+    res.json(post)
   })
 })
 
