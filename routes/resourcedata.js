802eb70cd23d4ea6393603ea52e29b39691e3709
Lovepreet Singh <lovepreet.singh@nagarro.com>
2018-06-27 15:37:16 +0530

    Added more apis

--- routes/resourcedata.js
+++ routes/resourcedata.js
@@ -11,22 +11,7 @@ router.get('/:id', (req, res, next) => {
   })
 })
 
-/* SAVE RESOURCE DATA*/
-router.post('/:id', (req, res, next) => {
-  ResourceData.find({}, ['resourceId'], { limit: 1, sort: { resourceId: -1 } }, (err, maxId) => {
-    if (maxId.length > 0 && maxId[0].resourceId)
-      req.body.resourceId = parseInt(maxId[0].resourceId) + 1
-    else
-      req.body.resourceId = 1;
-    ResourceData.create(req.body, (err, resource) => {
-      if (err) return next(err)
-      res.json(resource)
-    })
-  })
-})
-
-
-/* UPATE RESOURCE DATA*/
+/* UPATE RESOURCE DATA BY ID*/
 router.put('/:id', (req, res, next) => {
   ResourceData.findOneAndUpdate({ resourceId: req.params.id },
     req.body, { upsert: true }, (err, resource) => {
@@ -35,12 +20,5 @@ router.put('/:id', (req, res, next) => {
     })
 })
 
-/* REMOVE RESOURCE DATA*/
-router.delete('/:id', (req, res, next) => {
-  ResourceData.remove({ resourceId: req.params.id }, (err, resource) => {
-    if (err) return next(err)
-    res.json(resource)
-  })
-})
 
 module.exports = router;  
