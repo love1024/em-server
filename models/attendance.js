02e2dc0fb448cd85a95849423ebd5f7c6812af7d
Lovepreet Singh <lovepreet.singh@nagarro.com>
2018-06-28 12:41:58 +0530

    Added required fields

--- models/attendance.js
+++ models/attendance.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema
 
 module.exports = mongoose.model('ATTENDANCE', new Schema({
   attendanceId: Number,
-  startDate: { type: Date, default: Date.now },
-  endDate: { type: Date, default: Date.now },
-  attendanceType: String,
-  approvedBy: String,
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  attendanceType: { type: String, required: true },
+  approvedBy: { type: String, required: true },
   approvalDate: { type: Date, default: Date.now },
-  leaveType: String,
-  reason: String,
-  C_A_D: { type: Date, default: Date.now }
+  leaveType: { type: String, default: "" },
+  reason: { type: String, required: true },
+  clientApprovalDate: { type: Date, default: Date.now }
 }))
--- models/department.js
+++ models/department.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 module.exports = mongoose.model('DEPARTMENT', new Schema({
-  departmentId: { type: Number, required: true },
+  departmentId: { type: Number },
   departmentName: { type: String, required: true },
   departmentHod: { type: String, required: true },
   departmentHodEmail: { type: String, required: true },
