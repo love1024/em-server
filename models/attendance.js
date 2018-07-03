f9d70de8e86e6df66f93fb9f7f88d1cca52fbb1d
Lovepreet Singh <lovepreet.singh@nagarro.com>
2018-07-03 15:10:40 +0530

    changed resource model

--- models/attendance.js
+++ models/attendance.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+
 module.exports = mongoose.model('ATTENDANCE', new Schema({
   attendanceId: Number,
   resourceId: { type: Number, required: true },
